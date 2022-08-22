357cea383e50189fa87ae9d134f4f070d8cbd6d4
ychahbar19 <ychahbar19@gmail.com>
2022-08-22 20:17:05 +0200

    wip: database + schema definition

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { GameController } from './modules/game/app.controller';
 import { GameService } from './modules/game/app.service';
@@ -9,7 +10,7 @@ import { SummonerController } from './modules/summoner/app.controller';
 import { SummonerService } from './modules/summoner/app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController, GameController, SummonerController],
   providers: [AppService, GameService, SummonerService],
 })
