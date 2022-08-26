ee8aa69a421e9e1c2320cd24c46b2c7d3d80416e
ychahbar19 <ychahbar19@gmail.com>
2022-08-26 21:50:19 +0200

    fix: prisma schema + signup logic

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
-  login() {
+  login(body: AuthDto) {
+    console.log(body);
     return 'user logged in';
   }
 
-  signup(body) {
-    console.log(body);
-
+  async signup(body: AuthDto) {
+    // generate the password
+    const hash: string = await argon.hash(body.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: body.email,
+        username: body.username,
+        password: hash,
+      },
+    });
+    // return the new user
+    console.log(user);
     return 'user signed up';
   }
 }
