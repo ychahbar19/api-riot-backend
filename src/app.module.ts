b18f919b9aff694ff147ceb895d7303b994f6cdf
ychahbar19 <ychahbar19@gmail.com>
2022-09-16 09:42:45 +0200

    feat: adding redis cache configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
-import { AuthModule } from './modules/auth/auth.module';
+//import redisStore from 'cache-manager-redis-store';
+import * as redisStore from 'cache-manager-redis-store';
+
 import { PrismaModule } from './prisma/prisma.module';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { SummonerModule } from './modules/summoner/summoner.module';
 
 // PROVIDERS
 import { ConfigService } from './config/config.service';
 import { AtGuard } from './guards/index';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    SummonerModule,
+  ],
   controllers: [],
   providers: [
     {
