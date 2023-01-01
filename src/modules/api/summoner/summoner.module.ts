c748fbca8e8a49432e0828d64de735bf84abd7d3
ychahbar19 <ychahbar19@gmail.com>
2023-01-01 17:21:22 +0100

    feat: summonerModule as an auto injectable service

--- src/modules/api/summoner/summoner.module.ts
+++ src/modules/api/summoner/summoner.module.ts
@@ -7,5 +7,6 @@ import { SummonerService } from './summoner.service';
   imports: [HttpModule],
   controllers: [SummonerController],
   providers: [SummonerService],
+  exports: [SummonerService],
 })
 export class SummonerModule {}
