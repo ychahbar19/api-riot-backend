6dfdd93d64f91a59a93308b1d1955ee3e23988dd
ychahbar19 <ychahbar19@gmail.com>
2023-01-01 17:17:13 +0100

    feat: redis service & config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,34 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
-//import redisStore from 'cache-manager-redis-store';
 import * as redisStore from 'cache-manager-redis-store';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
 
 import { AuthModule } from './modules/api/auth/auth.module';
 import { UserModule } from './modules//api/user/user.module';
 import { SummonerModule } from './modules/api/summoner/summoner.module';
 
-import { SummonerModule as SummonerDtoModule } from './modules/rdo/summoner/summoner.module';
+import { SummonerRdoModule } from './modules/rdo/summoner/summoner.module';
 
 // PROVIDERS
 import { ConfigService } from './config/config.service';
 import { AtGuard } from './guards/index';
+import { HttpModule } from '@nestjs/axios';
 
-const dtoModules = [SummonerDtoModule];
+const dtoModules = [SummonerRdoModule];
 const apiModules = [AuthModule, UserModule, SummonerModule];
 @Module({
   imports: [
+    HttpModule,
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
       host: 'localhost',
       port: 6379,
     }),
+    RedisModule,
     PrismaModule,
     ...dtoModules,
     ...apiModules,
